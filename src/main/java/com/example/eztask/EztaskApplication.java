a894f94cde0e5136dda8820222aa3905d7237a0f
Home <cz2123zc@gmail.com>
2024-09-13 12:51:26 +0900

    Initial commit

---
+++ src/main/java/com/example/eztask/EztaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.eztask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EztaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EztaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eztask/EztaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eztask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EztaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
